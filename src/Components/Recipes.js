bba527d43552f602da438dc44908b09c4ea0793c
Mohammadreza <104798944+Mohammadrezanafar@users.noreply.github.com>
2023-09-08 15:19:27 +0100

    add image for header

--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -10,33 +10,35 @@ function Form() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      <label>
-        Surname:
-        <input
-          type="text"
-          value={surname}
-          onChange={(e) => setSurname(e.target.value)}
-        />
-      </label>
-      <label>
-        Age:
-        <input
-          type="text"
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+    <div className="info-users">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Surname:
+          <input
+            type="text"
+            value={surname}
+            onChange={(e) => setSurname(e.target.value)}
+          />
+        </label>
+        <label>
+          Age:
+          <input
+            type="text"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 export default Form;
